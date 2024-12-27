02a03b8ecf96699a1ef2a3bd0e77ff104b23472c
ProjectThor <WildCraftersMC@gmail.com>
2024-12-26 21:54:12 -0800

    initial commit

---
+++ src/main/java/net/projectthor/stringlights/StringLightsClient.java
@@ -0,0 +1,10 @@
+package net.projectthor.stringlights;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StringLightsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/projectthor/stringlights/StringLightsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.projectthor.stringlights;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class StringLightsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
